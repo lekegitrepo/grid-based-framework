76395b04c1ed193dd8525b9b36b2fc61511c48bc
ADELEKE TEMITAYO <lekedevrepo@gmail.com>
2019-08-30 09:48:21 -0600

    fix stickler errors

--- stylelint.config.js
+++ stylelint.config.js
@@ -10,6 +10,9 @@ module.exports = {
       ],
       ignore: ["after-comment"],
     } ],
+    "at-rule-no-unknown": [true, {
+     "ignoreAtRules": ["for", "each"]
+   }],
     "at-rule-name-case": "lower",
     "at-rule-name-space-after": "always-single-line",
     "at-rule-semicolon-newline-after": "always",
